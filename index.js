31388fc63984726fb9ca6bd91d020e705aa21a2b
Ty Rickers <ty.rickers@gmail.com>
2022-09-20 13:42:46 -0600

    added the image and centered it at the middle bottom of the screen

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+const image = document.getElementById("source");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Player {
+class Hero {
   constructor() {
-    console.log("this", this.height);
-    console.log("height", canvas.height - this.height);
-    this.position = {
-      //   x: 200,
-      x: canvas.width / 2,
-      //   y: canvas.height - this.height - 10,
-      y: canvas.height,
-    };
-
     this.velocity = {
       x: 0,
       y: 0,
     };
 
-    // this.image =
-    this.width = 50;
-    this.height = 50;
+    const image = new Image();
+
+    image.src = "./images/fs.png";
+    image.onload = () => {
+      const scale = 0.5;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height,
+      };
+    };
   }
 
   draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    if (this.image) {
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+    }
   }
 }
 
-const player = new Player();
+const player = new Hero();
 player.draw();
 
 function animate() {
