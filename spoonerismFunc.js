404853764a5b83463be2a62c0cdf83dff4746124
Ty Rickers <ty.rickers@gmail.com>
2023-01-25 20:41:44 -0700

    worked more on spoonerism func

--- spoonerismFunc.js
+++ spoonerismFunc.js
@@ -2,26 +2,52 @@
 //can make functions for kniferisms (end change) and forkerisms (middle vowel change) too
 
 // const CONSONANTS = /[(?![aeiou])[a-z]]/gi;
+const VOWELS = "aeiouAEIOU";
 
 const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
-  splitWordArray.map(async (spoonerWord, index) => {
-    if (index === splitWordArray.length - 1) return;
+  for (let i = 0; i < splitWordArray.length - 1; i++) {
+    let firstWord = splitWordArray[i];
+    let secondWord = splitWordArray[i + 1];
 
-    let secondWord = splitWordArray[index + 1];
+    for (let j = 0; j < 3; j++) {
+      let letterOfFirstWord = firstWord[j];
+      let letterOfSecondWord = secondWord[j];
 
-    let firstWordOneLetterReplaced = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${
-        secondWord[0] + spoonerWord.slice(1)
-      }`
-    );
-    let firstData = await firstWord.json();
+      if (
+        VOWELS.includes(letterOfFirstWord) ||
+        VOWELS.includes(letterOfSecondWord)
+      ) {
+        if (j === 0) {
+          break;
+        } else if (
+          !VOWELS.includes(letterOfFirstWord) &&
+          !VOWELS.includes(letterOfSecondWord)
+        ) {
+          continue;
+        } else if (
+          !VOWELS.includes(letterOfFirstWord) &&
+          VOWELS.includes(letterOfSecondWord)
+        ) {
+          if (VOWELS.includes(firstWord[j + 1])) {
+            firstWord = secondWord.splice(j); // need to think on this...
+          }
+        }
+      }
+    }
 
-    let secondWordOneLetterReplaced = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${
-        spoonerWord[0] + secondWord.slice(1)
-      }`
-    );
-    let secondData = await secondWord.json();
+    // let firstWordOneLetterReplaced = await fetch(
+    //     `https://api.dictionaryapi.dev/api/v2/entries/en/${
+    //         secondWord[0] + spoonerWord.slice(1)
+    //     }`
+    //     );
+    //     let firstData = await firstWord.json();
+
+    //     let secondWordOneLetterReplaced = await fetch(
+    //         `https://api.dictionaryapi.dev/api/v2/entries/en/${
+    //             spoonerWord[0] + secondWord.slice(1)
+    //         }`
+    //         );
+    //         let secondData = await secondWord.json();
 
     if (!firstData[0] || !secondData[0]) return;
 
@@ -29,10 +55,9 @@ const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
       splitWordArray[index] = `Ð${splitWordArray[index]}Ð`;
       splitWordArray[index + 1] = `Ð${splitWordArray[index + 1]}Ð`;
     }
-
-    console.log(secondWord[0] + spoonerWord.slice(1));
-    console.log(spoonerWord[0] + secondWord.slice(1));
-  });
+  }
+  console.log(secondWord[0] + spoonerWord.slice(1));
+  console.log(spoonerWord[0] + secondWord.slice(1));
 };
 
 // console.log(wrapSpoonerismWithUniqueCharacter(["butt", "just", "cut", "go"]));
