426031c73f1241446cfe0066ca17f7212b998529
Ty Rickers <ty.rickers@gmail.com>
2023-01-30 04:48:43 -0700

    added more to spoonerism func. still needs work

--- spoonerismFunc.js
+++ spoonerismFunc.js
@@ -1,7 +1,8 @@
 //wrap the spooner word in characters depending on if it has one, two, or three letters taken from the beginning
 //can make functions for kniferisms (end change) and forkerisms (middle vowel change) too
 
-const VOWELS = "aeiouAEIOU";
+//Add in the surrounding character as well that way the function doesn't create spoonerism on an already spoonerized word
+const VOWELS = "aeiouAEIOUÐ";
 
 const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
   for (let i = 0; i < splitWordArray.length - 1; i++) {
@@ -22,8 +23,10 @@ const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
           console.log("break");
           break;
         } else {
-          splitWordArray[i] = secondWord.slice(0, j) + firstWord.slice(j);
-          splitWordArray[i + 1] = firstWord.slice(0, j) + secondWord.slice(j);
+          splitWordArray[i] = `Ð${secondWord.slice(0, j) + firstWord.slice(j)}`;
+          splitWordArray[i + 1] = `${
+            firstWord.slice(0, j) + secondWord.slice(j)
+          }Ð`;
         }
       } else if (
         !VOWELS.includes(letterOfFirstWord) &&
@@ -36,17 +39,31 @@ const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
         (VOWELS.includes(letterOfSecondWord) || letterOfSecondWord === "y")
       ) {
         if (VOWELS.includes(firstWord[j + 1])) {
-          splitWordArray[i] = secondWord.slice(0, j) + firstWord.slice(j);
-          splitWordArray[i + 1] = firstWord.slice(0, j) + secondWord.slice(j);
-          console.log({ splitWordArray });
+          splitWordArray[i] = `Ð${secondWord.slice(0, j) + firstWord.slice(j)}`;
+          splitWordArray[i + 1] = `${
+            firstWord.slice(0, j) + secondWord.slice(j)
+          }Ð`;
+        } else if (
+          VOWELS.includes(firstWord[j + 1]) ||
+          letterOfFirstWord === "y"
+        ) {
+          splitWordArray[i] = `Ð${
+            secondWord.slice(0, j) + firstWord.slice(j + 1)
+          }`;
+          splitWordArray[i + 1] = `${
+            firstWord.slice(0, j + 1) + secondWord.slice(j)
+          }Ð`;
         }
+        console.log({ splitWordArray });
       } else if (
         (VOWELS.includes(letterOfFirstWord) || letterOfFirstWord === "y") &&
         !VOWELS.includes(letterOfSecondWord)
       ) {
-        if (VOWELS.includes(secondWord[j + 1])) {
-          splitWordArray[i] = secondWord.slice(0, j) + firstWord.slice(j);
-          splitWordArray[i + 1] = firstWord.slice(0, j) + secondWord.slice(j);
+        if (VOWELS.includes(secondWord[j + 1]) || letterOfFirstWord === "y") {
+          splitWordArray[i] = `Ð${secondWord.slice(0, j) + firstWord.slice(j)}`;
+          splitWordArray[i + 1] = `${
+            firstWord.slice(0, j) + secondWord.slice(j)
+          }Ð`;
         }
       }
     }
@@ -55,5 +72,12 @@ const wrapSpoonerismWithUniqueCharacter = (splitWordArray) => {
 };
 
 console.log(
-  wrapSpoonerismWithUniqueCharacter(["auto", "butt", "just", "chut", "go"])
+  wrapSpoonerismWithUniqueCharacter([
+    "auto",
+    "butt",
+    "just",
+    "chut",
+    "go",
+    "stripe",
+  ])
 );
