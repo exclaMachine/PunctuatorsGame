d02d0a3c00b0eb4ec5d5689462d9d958d35bc087
Ty Rickers <ty.rickers@gmail.com>
2023-10-19 20:11:47 -0600

    started implementation of new article function

--- articleFunc.js
+++ articleFunc.js
@@ -9,3 +9,38 @@ export const shakeAndBorderizeArticle = (node) => {
     node.className = "laughing-article-an";
   }
 };
+
+function highlightAndClassifyArticles(str) {
+  return str.replace(
+    /\b(the|an|a)\b(\s*\w?)/gi,
+    function (match, article, nextChar) {
+      let dataAlternate;
+      switch (article.toLowerCase()) {
+        case "a":
+        case "an":
+          dataAlternate = "the";
+          break;
+        case "the":
+          if (nextChar && "aeiouAEIOU".includes(nextChar.trim())) {
+            dataAlternate = "an";
+          } else {
+            dataAlternate = "a";
+          }
+          break;
+      }
+
+      // Match the case
+      if (article.charAt(0) === article.charAt(0).toUpperCase()) {
+        dataAlternate =
+          dataAlternate.charAt(0).toUpperCase() + dataAlternate.slice(1);
+      }
+
+      return `<span data-alternate="${dataAlternate}">${article}</span>${nextChar}`;
+    }
+  );
+}
+
+// Example usage:
+// const inputString = "A cat and an owl sat on the roof. The apple is sweet.";
+// const outputString = highlightAndClassifyArticles(inputString);
+// console.log(outputString);
