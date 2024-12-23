76fc573745c1991171403ffc4e7fd9537f7b4b23
Ty Rickers <ty.rickers@gmail.com>
2024-12-23 07:58:34 -0700

    redid event listener for keydown and keyup

--- IPA-fan-game/ipaFan.js
+++ IPA-fan-game/ipaFan.js
@@ -42,14 +42,6 @@ class Player {
   }
 }
 
-const map = [
-  ["-", "-", "-", "-", "-", "-"],
-  ["-", " ", " ", " ", " ", "-"],
-  ["-", " ", "-", "-", " ", "-"],
-  ["-", " ", " ", " ", " ", "-"],
-  ["-", "-", "-", "-", "-", "-"],
-];
-
 const boundaries = [];
 const player = new Player({
   position: {
@@ -61,6 +53,29 @@ const player = new Player({
     y: 0,
   },
 });
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const map = [
+  ["-", "-", "-", "-", "-", "-"],
+  ["-", " ", " ", " ", " ", "-"],
+  ["-", " ", "-", "-", " ", "-"],
+  ["-", " ", " ", " ", " ", "-"],
+  ["-", "-", "-", "-", "-", "-"],
+];
 
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -88,6 +103,18 @@ function animate() {
   });
 
   player.update();
+  player.velocity.x = 0;
+  player.velocity.y = 0;
+
+  if (keys.w.pressed) {
+    player.velocity.y = -5;
+  } else if (keys.a.pressed) {
+    player.velocity.x = -5;
+  } else if (keys.s.pressed) {
+    player.velocity.y = 5;
+  } else if (keys.d.pressed) {
+    player.velocity.x = 5;
+  }
 }
 
 animate();
@@ -95,16 +122,33 @@ animate();
 window.addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "w":
-      player.velocity.y = -5;
+      keys.w.pressed = true;
       break;
     case "a":
-      player.velocity.x = -5;
+      keys.a.pressed = true;
       break;
     case "s":
-      player.velocity.y = 5;
+      keys.s.pressed = true;
       break;
     case "d":
-      player.velocity.x = 5;
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
       break;
   }
 });
