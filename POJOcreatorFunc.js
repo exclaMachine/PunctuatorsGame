3845f5dca844dc1d2b680d22889adce07b31a30e
Ty Rickers <ty.rickers@gmail.com>
2025-03-15 15:09:39 -0600

    added m to mappings

--- POJOcreatorFunc.js
+++ POJOcreatorFunc.js
@@ -79,6 +79,7 @@ let roundedLetterPairs = {
   l: "c",
   t: "j",
   v: "u",
+  y: "m",
 };
 
 const ambigram = (word, pairs) => {
@@ -326,7 +327,7 @@ const CreateJS = (jsName, typeOfJSFunction) => {
 //CreateJS("ambigramPOJO.js", "ambigram");
 // CreateJS("todbotPOJO.js", "mirror");
 //CreateJS("todbotHorizontalPOJO.js", "sideMirror");
-//CreateJS("roundLetters.js", "roundLetters");
-CreateJS("roundLettersMulti.js", "roundLettersMulti");
+CreateJS("roundLetters.js", "roundLetters");
+//CreateJS("roundLettersMulti.js", "roundLettersMulti");
 
 //CreateJSON("todbotWithCapitals.json");
