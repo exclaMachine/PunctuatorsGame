69ea04c1977cac1a5065d9dd7f206c99d734712c
Ty Rickers <ty.rickers@gmail.com>
2025-11-17 07:46:12 -0700

    removed previous ambigrams and added all

--- anagrams_builder.js
+++ anagrams_builder.js
@@ -125,6 +125,6 @@ function CreateAnagramsJS(jsName, startLetters = null, opts = {}) {
   );
 }
 
-CreateAnagramsJS("anagrams.js", ["s"]);
+CreateAnagramsJS("anagrams.js");
 
 module.exports = { CreateAnagramsJS };
